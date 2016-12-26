c5e9e6f81be41b8e55fb26fa28eae4b15bf253ab
YunnaRulkes <thasmarinho@gmail.com>
2016-12-26 17:14:08 -0300

    update dht11

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,41 +1,51 @@
-/* NodeMCU and Blynk App*/
-#define BLYNK_PRINT Serial    
+/* NodeMCU and Blynk App*/   
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #define SSID "Erva_de_Gato"
 #define PASS "gatos_negros_1234"
 #define AUTH "5b7d55aed6064a0ab4ed9168afa68a65"
 
-/* DHT11 */
-#include "DHT.h"
-#define DHTPIN D3  
-#define DHTTYPE DHT11 
-DHT dht(DHTPIN, DHTTYPE);
-float hum = 0, temp = 0; 
-
 /* TIMER */
 #include <SimpleTimer.h>
 SimpleTimer timer;
 
-/* Soil Moister */
-#define soilMoisterPin A0
-#define soilMoisterVcc D4
-int soilMoister = 0;
-
-
-void FazConexaoWiFi(void);
-float FazLeituraUmidade(void);
+/* DHT11 */
+#include "DHT.h"
+#define DHTPIN 7 //D7 = GPIO13
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+float hum = 0, temp = 0; 
 
 void FazConexaoWiFi(void)
 { 
-    delay(1000);
+    delay(100);
     Blynk.begin(AUTH, SSID, PASS);
-    delay(1000);
+    delay(100);
 }
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(D7, OUTPUT);
   FazConexaoWiFi(); 
+    // Setup a function to be called every second
+  timer.setInterval(1000L, sendUptime);
+}
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendUptime()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(10, dht.readTemperature()); //virtual pin
+  Blynk.virtualWrite(11, dht.readHumidity()); // virtual pin 
+}
+
+void loop()
+{
+  Blynk.run(); // Initiates Blynk
+  timer.run(); // Initiates SimpleTimer
+  int chk; 
+  chk = dht.read(DHTPIN);    // READ DATA
 }
