01e3ca0d57cebfa99c2e235b6d334a98b23fa0dd
YunnaRulkes <thasmarinho@gmail.com>
2016-12-30 23:42:00 -0300

    add LM35 and 4051 tests

--- Tests/LM35WithBlynk/LM35WithBlynk.ino
+++ Tests/LM35WithBlynk/LM35WithBlynk.ino
@@ -5,7 +5,7 @@
 #define PASS "gatos_negros_1234"
 #define AUTH "5b7d55aed6064a0ab4ed9168afa68a65"
 #define sensor  A0      //Pino analógico em que o sensor está conectado.
-int valorSensor = 0; //Usada para ler o valor do sensor em tempo real.
+int valorSensor = 0; //Usada para ler o valor do sensor em valorSensoro real.
 
 void FazConexaoWiFi(void)
 { 
@@ -26,9 +26,9 @@ void setup(){
 void loop(){
   Blynk.run();
   //Lendo o valor do sensor.
-  int valorSensor = analogRead(sensor);
+  valorSensor = (float(analogRead(sensor))*3.3/(1023))/0.01; // costance (1/1024*100)
 
-  Blynk.virtualWrite(13, valorSensor);
+  Blynk.virtualWrite(14, valorSensor);
   Serial.println(valorSensor);
-
+  delay(2000);
 }
